e3503212cd2e3bcf05e21e1b10a987dc29be8573
AndrijZubal <andrij.zubal@gmail.com>
2021-01-20 22:14:21 +0200

    add task2

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import css from "./css/style.css";
 import task1 from "./task1.js"
+import task2 from "./task2.js"
---
+++ src/task2.js
@@ -0,0 +1,45 @@
+// Перепиши функцию toggleUserState() так,
+//     чтобы она не использовала callback - функцию callback,
+//     а принимала всего два параметра allUsers и userName
+// и возвращала промис.
+
+import { resolve } from "q";
+
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+  
+    return new Promise((resolve) => {
+        setTimeout(() => {
+  const updatedUsers = allUsers.map(user =>
+    user.name === userName ? { ...user, active: !user.active } : user,
+  );
+  
+  resolve(updatedUsers)
+        }, 0);
+    });
+//   const updatedUsers = allUsers.map(user =>
+//     user.name === userName ? { ...user, active: !user.active } : user,
+//   );
+
+//   callback(updatedUsers);
+};
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+/*
+ * Сейчас работает так
+ */
+// toggleUserState(users, 'Mango', logger);
+// toggleUserState(users, 'Lux', logger);
+
+/*
+ * Должно работать так
+ */
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
